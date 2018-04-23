009e032bb762c4a02eecfe09684f68172e998090
Allan Wang <me@allanwang.ca>
2018-04-23 01:10:47 -0400

    Use built in lazy method, resolves #1

--- props/src/main/kotlin/ca/allanwang/kit/props/PropHolder.kt
+++ props/src/main/kotlin/ca/allanwang/kit/props/PropHolder.kt
@@ -2,7 +2,6 @@ package ca.allanwang.kit.props
 
 import java.io.File
 import java.util.*
-import kotlin.reflect.KProperty
 
 /**
  * Prop container
@@ -32,73 +31,22 @@ open class PropHolder(val props: Properties) {
     /**
      * Prop delegation to return an int or default
      */
-    fun int(key: String, default: Int = 0, errorMessage: String? = null): Lazy<Int> =
-            SynchronizedLazyProp({
-                nonnull(get(key)?.toIntOrNull(), default, errorMessage)
-            })
+    fun int(key: String, default: Int = 0, errorMessage: String? = null): Lazy<Int> = lazy {
+        nonnull(get(key)?.toIntOrNull(), default, errorMessage)
+    }
 
     /**
      * Prop delegation to return a nonnull string or default
      */
-    fun string(key: String, default: String = "", errorMessage: String? = null): Lazy<String> =
-            SynchronizedLazyProp({
-                nonnull(get(key), default, errorMessage)
-            })
+    fun string(key: String, default: String = "", errorMessage: String? = null): Lazy<String> = lazy {
+        nonnull(get(key), default, errorMessage)
+    }
 
     /**
      * Prop delegation to return a nullable string
      */
-    fun nullableString(key: String): Lazy<String?> =
-            SynchronizedLazyProp({
-                get(key)
-            })
-
-}
-
-private class StringProp {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return "$thisRef, thank you for delegating '${property.name}' to me!"
+    fun nullableString(key: String): Lazy<String?> = lazy {
+        get(key)
     }
-
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
-        println("$value has been assigned to '${property.name}' in $thisRef.")
-    }
-}
-
-
-private object UNINITIALIZED_PROP
-
-private class SynchronizedLazyProp<out T>(initializer: () -> T, lock: Any? = null) : Lazy<T> {
-    private var initializer: (() -> T)? = initializer
-    @Volatile
-    private var _value: Any? = UNINITIALIZED_PROP
-    // final field is required to enable safe publication of constructed instance
-    private val lock = lock ?: this
-
-    override val value: T
-        get() {
-            val _v1 = _value
-            if (_v1 !== UNINITIALIZED_PROP) {
-                @Suppress("UNCHECKED_CAST")
-                return _v1 as T
-            }
-
-            return synchronized(lock) {
-                val _v2 = _value
-                if (_v2 !== UNINITIALIZED_PROP) {
-                    @Suppress("UNCHECKED_CAST") (_v2 as T)
-                } else {
-                    val typedValue = initializer!!()
-                    _value = typedValue
-                    initializer = null
-                    typedValue
-                }
-            }
-        }
-
-    override fun isInitialized(): Boolean = _value !== UNINITIALIZED_PROP
-
-    override fun toString(): String = if (isInitialized()) value.toString()
-    else "Prop value not initialized yet."
 
 }
